061238341bf927b9ed32e5577fe92f6913cf4df6
seh <02milkyway13@naver.com>
2023-02-11 19:26:24 +0900

    first

---
+++ project7012_ver3/src/main/java/com/project7012/Project7012Ver2Application.java
@@ -0,0 +1,13 @@
+package com.project7012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project7012Ver2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project7012Ver2Application.class, args);
+	}
+
+}
